d53e7043ce90631246f8cbd2bab26df7eeaee849
Sandhiya S <ss115605@bctchn.local>
2024-11-29 12:35:32 +0530

    source changes

---
+++ src/main/java/Bahwan/TestCoverageDemo/codeCoverage.java
@@ -0,0 +1,24 @@
+package Bahwan.TestCoverageDemo;
+
+public class codeCoverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello BCT");
+		start();
+
+	}
+	public static String start() {
+		System.out.println("start");
+		return "start";
+	}
+	
+	public static String stop() {
+		System.out.println("stop");
+		return "stop";
+	}
+public static String processing() {
+	System.out.println("process");
+	return "process";
+}
+}
---
+++ src/test/java/Bahwan/TestCoverageDemo/codeCoverageTest.java
@@ -0,0 +1,21 @@
+package Bahwan.TestCoverageDemo;
+
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class codeCoverageTest {
+
+	@Test
+	public void sample1() {
+
+		Assert.assertEquals(codeCoverage.start(), "start");
+
+	}
+
+	@Test
+	public void sample2() {
+		Assert.assertEquals(codeCoverage.stop(), "stop");
+	}
+}
